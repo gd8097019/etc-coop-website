673a3e87bb242e89cc3a4bc73899993c238c0cd0
agursoy <102132498+gd8097019@users.noreply.github.com>
2023-07-31 15:51:14 +0300

    gpdr popup added

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
 <template>
 	<router-view />
+	<vue-cookie-accept-decline
+		:debug="false"
+		:disableDecline="true"
+		:showPostponeButton="false"
+		position="bottom-left"
+		transitionName="slideFromBottom"
+		type="floating"
+	>
+		<!-- Optional -->
+		<template #postponeContent>&times;</template>
+
+		<!-- Optional -->
+		<template #message>
+			We use cookies to ensure you get the best experience on our website.
+			<a href="/cookie-policy" target="_blank">Learn More...</a>
+		</template>
+
+		<!-- Optional -->
+		<template #declineContent>Opt Out</template>
+
+		<!-- Optional -->
+		<template #acceptContent>Got It!</template>
+	</vue-cookie-accept-decline>
 </template>
