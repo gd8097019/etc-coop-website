e454bef815f89894390e6c54e0e69e1c74693bff
gd8097019 <gd8097019@gmail.com>
2023-08-29 20:06:00 +0530

    Revised Filing page structure

--- src/views/Filings.vue
+++ src/views/Filings.vue
@@ -12,200 +12,9 @@
 		</template>
 
 		<template #main>
-			<!-- incorporation documents section starts here -->
-			<section class="documentsSection filingsPage">
-				<div class="documentsContainer">
-					<div class="heading">
-						<h1>{{ $t("filings.incorporation_documents") }}</h1>
-					</div>
-					<div class="accordion" id="accordionPanelsStayOpenExample">
-						<div
-							class="accordion-item"
-							v-for="(incorpation, ikey) in incorpations"
-							:key="ikey"
-						>
-							<h2 class="accordion-header">
-								<button
-									class="accordion-button"
-									:class="{ collapsed: ikey !== 0 ? true : false }"
-									type="button"
-									data-bs-toggle="collapse"
-									:data-bs-target="`#collapse-i-${ikey}`"
-									aria-expanded="false"
-									:aria-controls="`collapse-i-${ikey}`"
-									@click="updateAccordionText('i', ikey)"
-								>
-									<span class="accBtnContent">
-										{{ incorpation.title }}
-										<span v-if="ikey === 0" class="showAndHide">{{
-											$t("filings.hide")
-										}}</span>
-										<span v-else class="showAndHide">{{
-											$t("filings.show")
-										}}</span>
-									</span>
-								</button>
-							</h2>
-							<div
-								:id="`collapse-i-${ikey}`"
-								class="accordion-collapse collapse accordion-collapse collapse"
-								:class="{
-									show: activeIncorporationTab === ikey ? true : false,
-								}"
-							>
-								<div
-									v-for="(file, fkey) in incorpation.files"
-									:key="fkey"
-									class="documentCard"
-								>
-									<a
-										target="_blank"
-										:href="file.fileLink"
-										:download="file.date"
-									>
-										<img
-											class="pdfIcon"
-											src="@/assets/images/pdf-icon.svg"
-											alt=""
-										/>
-									</a>
-									<div class="documentCardContent">
-										<div class="documentSubj">
-											<h3 v-if="file.date !== ''">{{ file.date }}</h3>
-											<a
-												target="_blank"
-												:href="file.fileLink"
-												:download="file.date"
-											>
-												<p>
-													{{ file.description }}
-												</p>
-											</a>
-										</div>
-										<a
-											target="_blank"
-											:href="file.fileLink"
-											:download="file.date"
-										>
-											<img
-												class="lightIcon"
-												src="@/assets/images/arrow-right-dark.svg"
-												alt=""
-											/>
-											<img
-												class="darkIcon"
-												src="@/assets/images/arrow-right-gray.svg"
-												alt=""
-											/>
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-			<!-- incorporation documents section ends here -->
-
-			<!-- resolutions section starts here -->
-			<section class="documentsSection filingsPage">
-				<div class="documentsContainer">
-					<div class="heading">
-						<h1>{{ $t("filings.resolutions") }}</h1>
-					</div>
-					<div class="accordion" id="accordionPanelsStayOpenExample">
-						<div
-							class="accordion-item"
-							v-for="(resolution, rkey) in resolutions"
-							:key="rkey"
-						>
-							<h2 class="accordion-header">
-								<button
-									class="accordion-button"
-									:class="{ collapsed: rkey !== 0 ? true : false }"
-									type="button"
-									data-bs-toggle="collapse"
-									:data-bs-target="`#collapse-r-${rkey}`"
-									aria-expanded="false"
-									:aria-controls="`collapse-r-${rkey}`"
-									@click="updateAccordionText('r', rkey)"
-								>
-									<span class="accBtnContent">
-										{{ resolution.title }}
-										<span v-if="rkey === 0" class="showAndHide">{{
-											$t("filings.hide")
-										}}</span>
-										<span v-else class="showAndHide">{{
-											$t("filings.show")
-										}}</span>
-									</span>
-								</button>
-							</h2>
-							<div
-								:id="`collapse-r-${rkey}`"
-								class="accordion-collapse collapse accordion-collapse collapse"
-								:class="{
-									show: activeResolutionsTab === rkey ? true : false,
-								}"
-							>
-								<div
-									v-for="(file, fkey) in resolution.files"
-									:key="fkey"
-									class="documentCard"
-								>
-									<a
-										target="_blank"
-										:href="file.fileLink"
-										:download="file.date"
-									>
-										<img
-											class="pdfIcon"
-											src="@/assets/images/pdf-icon.svg"
-											alt=""
-										/>
-									</a>
-									<div class="documentCardContent">
-										<div class="documentSubj">
-											<h3 v-if="file.date !== ''">{{ file.date }}</h3>
-											<a
-												target="_blank"
-												:href="file.fileLink"
-												:download="file.date"
-											>
-												<p>{{ file.description }}</p>
-											</a>
-										</div>
-										<a
-											target="_blank"
-											:href="file.fileLink"
-											:download="file.date"
-										>
-											<img
-												class="lightIcon"
-												src="@/assets/images/arrow-right-dark.svg"
-												alt=""
-											/>
-											<img
-												class="darkIcon"
-												src="@/assets/images/arrow-right-gray.svg"
-												alt=""
-											/>
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-			<!-- resolutions section ends here -->
-
 			<!-- filings section starts here -->
 			<section class="documentsSection filingsPage">
 				<div class="documentsContainer">
-					<div class="heading">
-						<h1>{{ $t("filings.filings") }}</h1>
-					</div>
 					<div class="accordion" id="accordionPanelsStayOpenExample">
 						<div
 							class="accordion-item"
@@ -296,9 +105,6 @@
 			<!-- other documents section starts here -->
 			<section class="otherDocsSections">
 				<div class="otherDocsContainer">
-					<div class="heading">
-						<h1>{{ $t("filings.other_documents") }}</h1>
-					</div>
 					<div class="row">
 						<div
 							v-for="(otherDocument, oKey) in otherDocuments"
@@ -361,20 +167,9 @@ export default {
 			}
 		},
 		getContent(lang) {
-			import(`@/config/${lang}/filing.incorpations.json`).then((contentEN) => {
-				this.incorpations = contentEN.default.incorpations;
-			});
-			import(`@/config/${lang}/filing.resolutions.json`).then((contentEN) => {
-				this.resolutions = contentEN.default.resolutions;
-			});
-			import(`@/config/${lang}/filing.filings.json`).then((contentEN) => {
+			import(`@/config/${lang}/filing.consolidated.json`).then((contentEN) => {
 				this.filings = contentEN.default.filings;
 			});
-			import(`@/config/${lang}/filing.other-documents.json`).then(
-				(contentEN) => {
-					this.otherDocuments = contentEN.default.otherDocuments;
-				}
-			);
 		},
 	},
 	created() {
