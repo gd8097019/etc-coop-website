4c0d673d3c345dc54c28400acf83621edb5164a2
gd8097019 <gd8097019@gmail.com>
2023-11-22 01:31:48 +0530

    Fixed search on mobile

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,5 +1,5 @@
 <template>
-    <img id="search" class="search-icon" src="@/assets/images/search.svg" data-bs-toggle="modal" data-bs-target="#exampleModal" />
+    <img id="search" class="search-icon" src="@/assets/images/search.svg" @click="showModal"  />
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -12,13 +12,36 @@
     </div>
 </template>
 <script>
-import { h, Fragment, render, onMounted } from 'vue';
+
+import { h, ref, Fragment, render, onMounted } from 'vue';
 import algoliasearch from 'algoliasearch/lite';
 import { autocomplete, getAlgoliaResults } from '@algolia/autocomplete-js';
 import '@algolia/autocomplete-theme-classic';
 export default {
     props: ['lang'],
     setup(props) {
+
+        const showModal = () => {
+            var el = document.getElementById('exampleModal');
+            el.className += ' show';
+            el.style.cssText = 'display: block;';
+            document.body.style.cssText = 'overflow: hidden; padding-right: 0px;';
+            var elemDiv = document.createElement('div');
+            elemDiv.className = 'modal-backdrop fade show';
+            document.body.appendChild(elemDiv);
+            document.querySelector('.modal').onclick = (e) => {
+                e.target.id == 'exampleModal' && hideModal();
+            }
+        }
+
+        const hideModal = () => {
+            var el = document.getElementById('exampleModal');
+            el.className = 'modal fade';
+            el.style.cssText = 'display: none;';
+            document.body.style.cssText = '';
+            document.querySelector('.modal-backdrop').remove()
+        }
+
         onMounted(() => {
             const searchClient = algoliasearch('7PCCLM6OLI', '5a455e80c51bc83e6b2b38611176c993');
             autocomplete({
@@ -76,6 +99,10 @@ export default {
                 },
             });
         });
+
+        return {
+            showModal, hideModal
+        }
     }
 }
 </script>
