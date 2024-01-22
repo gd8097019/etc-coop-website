e8a2bbd2b5abc1561079a0435c6cccef70dbb2c8
Andrew <andrewgdick@gmail.com>
2024-01-22 11:24:33 -0500

    Update Contact.vue Update Andrew Contact (#235)

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -135,7 +135,7 @@ export default {
 					name: "Andrew Dick",
 					title: "Marketing Manager",
 					mail: "andrew@etccooperative.org",
-					twitter: "Bubble_Gum_Tate",
+					
 				},
 				{
 					img: "angelah-liu.png",
